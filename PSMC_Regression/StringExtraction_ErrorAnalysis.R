a58a4e8f76b5df9374afde42c4c6f445b819431c
Alex Jackson <alex.jackson@student.adelaide.edu.au>
2016-01-20 15:34:56 +1030

    This program extracts details from a PSMC file name, does an error analysis versus various true plots, and puts output in a data frame.

---
+++ PSMC_Regression/StringExtraction_ErrorAnalysis.R
@@ -0,0 +1,121 @@
+library("stringr", lib.loc="~/R/x86_64-pc-linux-gnu-library/3.0")
+
+setwd("/home/alex/Desktop/Software/Alex/PSMC_Regression/emptydata/")
+filelist = list.files(pattern = "*.txt",recursive=T)
+
+############### TRUE POPULATION AND TIME VALUES AS SPECIFIED BY THE MS COMMANDS
+ms_eN_2_vec = function(ms_input, t_or_p) {
+# takes ms_input of form "-eN 0.0055 0.0832 -eN 0.0089 0.0489" copied from ms
+# t_or_p can be TRUE or FALSE. If TRUE, gives times. If FALSE, gives populations.
+  TimesPops = as.numeric(unlist(str_extract_all(gsub(pattern = "-eN ", replacement = "", x = ms_input), "[\\.0-9e-]+")))  # remove the -eN switch, convert to a vector
+  if (t_or_p == TRUE) {
+    Times = 4*TimesPops[c(TRUE,FALSE)] # extracts the odd values (i.e. the times) and scales
+    return(Times)
+  } # returning pops
+  else {
+    Pops = 4*TimesPops[c(FALSE,TRUE)] # extracts the even values (i.e. the pops) and scales
+    return(Pops)    
+  }
+}
+
+#FauxHumanMs_eN = "-eN 0.0055 0.0832 -eN 0.0089 0.0489 -eN 0.0130 0.0607 -eN 0.0177 0.1072 -eN 0.0233 0.2093 -eN 0.0299 0.3630 -eN 0.0375 0.5041 -eN 0.0465 0.5870 -eN 0.0571 0.6343 -eN 0.0695 0.6138 -eN 0.0840 0.5292 -eN 0.1010 0.4409 -eN 0.1210 0.3749 -eN 0.1444 0.3313 -eN 0.1718 0.3066 -eN 0.2040 0.2952 -eN 0.2418 0.2915 -eN 0.2860 0.2950 -eN 0.3379 0.3103 -eN 0.3988 0.3458 -eN 0.4701 0.4109 -eN 0.5538 0.5048 -eN 0.6520 0.5996 -eN 0.7671 0.6440 -eN 0.9020 0.6178 -eN 1.0603 0.5345 -eN 1.4635 1.7931" # as specified by ms
+#FauxHumanTimesPops = as.numeric(unlist(str_extract_all(gsub(pattern = "-eN ", replacement = "", x = FauxHumanMs_eN), "[\\.0-9e-]+")))  # remove the -eN switch, convert to a vector
+#FauxHumanTimes = 4*FauxHumanTimesPops[c(TRUE,FALSE)] # extracts the odd values (i.e. the times) and scales
+#FauxHumanTimes
+
+#FauxHumanPops = 4*FauxHumanTimesPops[c(FALSE, TRUE)] # extracts the even values (i.e. the pops) and scales
+
+# need to define true times and populations
+fauxHumanMs_eN = "-eN 0.0055 0.0832 -eN 0.0089 0.0489 -eN 0.0130 0.0607 -eN 0.0177 0.1072 -eN 0.0233 0.2093 -eN 0.0299 0.3630 -eN 0.0375 0.5041 -eN 0.0465 0.5870 -eN 0.0571 0.6343 -eN 0.0695 0.6138 -eN 0.0840 0.5292 -eN 0.1010 0.4409 -eN 0.1210 0.3749 -eN 0.1444 0.3313 -eN 0.1718 0.3066 -eN 0.2040 0.2952 -eN 0.2418 0.2915 -eN 0.2860 0.2950 -eN 0.3379 0.3103 -eN 0.3988 0.3458 -eN 0.4701 0.4109 -eN 0.5538 0.5048 -eN 0.6520 0.5996 -eN 0.7671 0.6440 -eN 0.9020 0.6178 -eN 1.0603 0.5345 -eN 1.4635 1.7931"
+fauxHumanTimes = ms_eN_2_vec(fauxHumanMs_eN,TRUE)
+fauxHumanPops = ms_eN_2_vec(fauxHumanMs_eN,FALSE)
+constantPopTimes = 4*c(0.01,1:100) # dummy times
+constantPopPops = 4*rep(1,length(constantPopTimes))
+psmcSim2MS_eN = "-eN 0.1 5 -eN 0.6 20 -eN 2 5 -eN 10 10 -eN 20 5"
+psmcSim2Times = ms_eN_2_vec(psmcSim2MS_eN,TRUE)
+psmcSim2Pops = ms_eN_2_vec(psmcSim2MS_eN,FALSE)
+
+
+##############################33
+# need to do a min of maxs, max of mins to determine integration limits
+logmins = rep(0,length(filelist)+1) # the plus one for the smallest (and largest) true times
+logmaxs = rep(0,length(filelist)+1)
+i = 1
+for (infile in filelist) {
+  data.infile = read.table(infile,header=TRUE)
+  logmins[i] = log(min(data.infile$t_k/2)) # gotta divide by 2 to scale PSMC time output to N_0 generations. THIS IS IMPORTANT OTHERWISE THE PROGRAMS STUFF! MUST DO WHENEVER THERE IS A t_k
+  logmaxs[i] = log(max(data.infile$t_k/2))
+  i = i+1
+}
+logmins[length(filelist)+1] = log(min(fauxHumanTimes,constantPopTimes,psmcSim2Times)) # add true value min
+logmaxs[length(filelist)+1] = log(max(fauxHumanTimes,constantPopTimes,psmcSim2Times)) # add true value max
+logmax_of_mins = max(logmins)
+logmin_of_maxs = min(logmaxs)
+
+# need to set up integration functions
+# this function will tell you PSMC's population size estimate at time "pos" given the x (time) and y (relative pop) data
+eval_popsize = function(pos, x, y){ 
+  xIndex = length(which(x <= pos))
+  return(y[xIndex])
+}
+
+# this function will tell you the absolute difference in between two curves A and B at position "xpos". we will later integrate this to get an area between two curves
+absdifference = function(xpos,d1,d2,d3,d4){ # d1, d2 are x and y of curve A. d1, d2 are x and y of curve B. you want to compare curves A and B.
+  out = NULL
+  n=length(xpos)
+  for (i in 1:n) {
+    out[i] = abs( eval_popsize(xpos[i],d1,d2) - eval_popsize(xpos[i],d3,d4) )
+  }
+  return(out)
+}
+
+# EXTRACT NUMBERS BETWEEN SPECIFIC WORDS! reference: http://stackoverflow.com/questions/22924183/extract-symbols-between-two-particular-words
+#numbaaaa = gsub(pattern = "(.*Split)(.*)(\\.psmcfa.*)",
+#     replacement = "\\2",
+#     x = filelist[1])
+#numbaaaa = as.numeric(numbaaaa)
+#numbaaaa
+
+#lettaaaa = gsub(pattern = "(.*)(Mbp.*)",
+#                replacement = "\\1",
+#                x = filelist[1])
+#lettaaaa
+
+#### END TEST
+
+# Initialise input variables
+Bp = NULL
+Int = NULL
+Split = NULL
+Bp_per_contig = NULL
+Pop_Dynamics_Type = NULL
+Error = NULL
+
+# files have names like "fauxHuman/fauxHumanBp100000Int10*1Split1/fauxHumanBp100000Int10*1Split1.txt"
+
+for (infile in filelist) { # make sure I change things to Shaun's file format
+  Bp_tmp = as.numeric(gsub(pattern = "(.*Bp)(.*)(Int.*)", replacement = "\\2", x = infile))
+  Bp = append(Bp,Bp_tmp)
+  Int = append(Int,as.numeric(gsub(pattern = "(.*Int)(.*)(\\*)(.*)(Split.*)", replacement = "\\2", x = infile))) # for something like 10*1, only extracts the 10 (we wouldn't do anything like x*y for y!= 1)
+  Split_tmp = as.numeric(gsub(pattern = "(.*Split)(.*)(\\.txt)", replacement = "\\2", x = infile))
+  Split = append(Split,Split_tmp)
+  Bp_per_contig = append(Bp_per_contig,ceiling(Bp_tmp/Split_tmp)) # to convert from Mbp to bp)
+  Pop_Dynamics_Type_tmp = gsub(pattern = "(\\/.*)(.*)(Bp.*)", replacement = "\\2", x = infile)
+  Pop_Dynamics_Type = append(Pop_Dynamics_Type,Pop_Dynamics_Type_tmp)
+  
+  # error analysis
+  data = read.table(infile,header=TRUE)
+  # need to make sure you integrate against the correct true curve
+  if (Pop_Dynamics_Type_tmp == "fauxHuman") {
+    Error = integrate(absdifference,logmax_of_mins,logmin_of_maxs, d1=log(fauxHumanTimes), d2=fauxHumanPops, d3=log(data$t_k/2), d4=data$lambda_k, subdivisions=10000)$value 
+  }
+  if (Pop_Dynamics_Type_tmp == "constantPop") {
+    Error = integrate(absdifference,logmax_of_mins,logmin_of_maxs, d1=log(constantPopTimes), d2=constantPopPops, d3=log(data$t_k/2), d4=data$lambda_k, subdivisions=10000)$value 
+  } 
+  if (Pop_Dynamics_Type_tmp == "psmcSim2") {
+    Error = integrate(absdifference,logmax_of_mins,logmin_of_maxs, d1=log(psmcSim2Times), d2=psmcSim2Pops, d3=log(data$t_k/2), d4=data$lambda_k, subdivisions=10000)$value
+  }
+} 
+Results = data.frame(Bp,Int,Split,Bp_per_contig,Pop_Dynamics_Type,Error)
+
+Results
